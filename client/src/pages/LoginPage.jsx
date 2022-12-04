83986bc660d18b669e1a9526185c257686f99170
philiplee13 <philipyjlee95@gmail.com>
2022-12-04 12:55:25 -0800

    modify client stuff

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import LoginForm from "../components/LoginForm";
 
 function LoginPage() {
     return (
       <div>
-      <button>Login</button>
+      <LoginForm/>
       </div>
     );
   }
